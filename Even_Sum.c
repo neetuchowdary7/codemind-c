7a70cc12b7d1cf8f0d2b774737af60496cad5217
neetuchowdary7 <147469951+neetuchowdary7@users.noreply.github.com>
2023-12-19 10:07:00 +0530

    #include<stdio.h> int main() {     int a,i;     int arr[a];     scanf("%d",&a);     for ( i=1 ; i<=a ; i++ )     {         scanf("%d ",&arr[i]);     }     int sum=0;     for ( i=1 ; i<=a ; i++ )     {      if ( arr[i] % 2 ==0)      sum+=arr[i];      }      printf("%d",sum); }

---
+++ Even_Sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,i;
+    int arr[a];
+    scanf("%d",&a);
+    for ( i=1 ; i<=a ; i++ )
+    {
+        scanf("%d ",&arr[i]);
+    }
+    int sum=0;
+    for ( i=1 ; i<=a ; i++ )
+    {
+     if ( arr[i] % 2 ==0)
+     sum+=arr[i];
+     }
+     printf("%d",sum);
+}
