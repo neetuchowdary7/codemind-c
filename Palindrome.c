20546ddb64cb6ab22b208878ed358f2e45f7cbf2
neetuchowdary7 <147469951+neetuchowdary7@users.noreply.github.com>
2023-12-19 11:30:11 +0530

    #include<stdio.h> int main() {     int i,n,t,rev=0;     scanf("%d",&n);     t=n;     while(n>0)     {         int r=n%10;         rev = rev*10+r;         n/=10;     }     if ( rev == t )     printf("Palindrome");     else     printf("Not Palindrome"); }

--- Palindrome.c
+++ Palindrome.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main()
 {
-	int n,r,rev=0,t;
-	scanf("%d",&n);
-	t=n;
-	while(n>0)
-	{
-		r=n%10;
-		rev=rev*10+r;
-		n=n/10;
-	}
-	if (rev==t)
-	printf("True");
-	else 
-	printf("False");
+    int i,n,t,rev=0;
+    scanf("%d",&n);
+    t=n;
+    while(n>0)
+    {
+        int r=n%10;
+        rev = rev*10+r;
+        n/=10;
+    }
+    if ( rev == t )
+    printf("Palindrome");
+    else 
+    printf("Not Palindrome");
 }
