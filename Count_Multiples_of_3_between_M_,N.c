6e6782a969019733901d57e7607abf033e4dd434
Gnana Sai Nivedita Babburi <147469951+neetuchowdary7@users.noreply.github.com>
2023-12-31 16:04:37 +0530

    #include<stdio.h> int main() {     int a,b,count=0;     int i;     scanf("%d%d",&a,&b);     for(i=a;i<=b;i++)     {         if( i % 3 == 0)         {             count++;         }     }     printf("%d",count); }

---
+++ Count_Multiples_of_3_between_M_,N.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,count=0;
+    int i;
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++)
+    {
+        if( i % 3 == 0)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
